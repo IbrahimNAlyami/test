7ae63d43b17686593068168d0099e42f31febe6c
IbrahimNAlyami <Ibrahim.nashir.eam.js@tuwaiq.edu.sa>
2022-01-10 10:13:41 +0300

    addes test branch

---
+++ test.js
@@ -0,0 +1,32 @@
+// let age = 24
+// if (age > 20){
+//     return "More than 20"
+// }else{
+//     return "Less than 20"
+// }
+
+// let price = 50
+// if (price >= 60){
+//     console.log("Expensive");
+// }
+// else if(price >= 30 && price <= 60){
+//     console.log("Affordable");
+// }
+// else if(price <= 30){
+//     console.log("Cheep");
+// }
+
+for(let i = 10; i >=1; i--){
+    console.log(i);
+}
+
+
+// $('#button').click(function (){
+//     alert("Button is clicked");
+// })
+
+// fetch("https://jsonplaceholder.typicode.com/todos/1")
+// .then((res) => res.json)
+// .then((data)=>{
+//     data.data
+// })
